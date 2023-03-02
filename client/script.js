bd372be535895bae50d392b5640a2cecbcdda1e1
Egor <vegormak7@gmail.com>
2023-03-02 20:34:14 +0300

    update server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ async function handleSubmit(e) {
   loader(messageDiv);
 
   // fetch response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://openai-chat-84ky.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
